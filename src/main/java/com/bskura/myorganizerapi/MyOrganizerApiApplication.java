2fbc51b07054c982e6de6b9ed21692dc6eb885d9
Bartłomiej Skura <52361028+BSkura98@users.noreply.github.com>
2024-02-13 18:43:04 +0100

    Initial commit

---
+++ src/main/java/com/bskura/myorganizerapi/MyOrganizerApiApplication.java
@@ -0,0 +1,13 @@
+package com.bskura.myorganizerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOrganizerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOrganizerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bskura/myorganizerapi/MyOrganizerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bskura.myorganizerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOrganizerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
